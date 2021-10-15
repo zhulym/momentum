ab5c76781fffc67bbbb3cf728e29b90d82ff2452
zhulym <zhulym@mail.ru>
2021-10-15 21:17:39 +0300

    feat: impl first part of audio player

--- momentum/scripts/index.js
+++ momentum/scripts/index.js
@@ -25,7 +25,144 @@ window.addEventListener('load', getWeather);
 changeQuoteBtn.addEventListener('click', getQuote);
 window.addEventListener('load', getQuote);
 
+////////////////////audio
+
+const audioItem = document.querySelector('.audio__item');
+const volumeRange = document.querySelector('#volume__range');
+const audioRange = document.querySelector('#audio__range');
+const playBtn = document.querySelector('.play');
+const volumeIcon = document.querySelector('.volume__icon');
+
+const prevBtn = document.querySelector('.play-prev');
+const nextBtn = document.querySelector('.play-next');
+
+const timeCurrent = document.querySelector('.time__current');
+const timeEnd = document.querySelector('.time__end');
+
+let buffer = document.querySelector('.time__buffer');
 
 
 
+let isPlay = false;
+let duration = audioItem.duration;
+let flagVolume = 1;
 
+playBtn.addEventListener("click", playStop);
+
+function playStop() {
+  if (isPlay === false) {
+    audioItem.play();
+    playBtn.classList.toggle('pause');
+    isPlay = true;
+  } else {
+    audioItem.pause();
+    playBtn.classList.toggle('pause');
+    isPlay = false;
+  }
+}
+
+audioItem.addEventListener("timeupdate", showProgress)
+
+function showProgress() {
+  timeCurrent.textContent = getTime(audioItem.currentTime);
+  timeEnd.textContent = getTime(audioItem.duration);
+  let barLength = (audioItem.currentTime / audioItem.duration) * 100;
+  audioRange.value = barLength;
+  let color = 'background : linear-gradient(to right,#fff ' + audioRange.value + '%,rgba(255,255,255,.5) ' + audioRange.value + '%)!important;';
+  audioRange.style = color;
+
+  if (duration > 0) {
+    for (let i = 0; i < audioItem.buffered.length; i++) {
+      if (audioItem.buffered.start(audioItem.buffered.length - 1 - i) < audioItem.currentTime) {
+        let audioItemBufWidth = (audioItem.buffered.end(audioItem.buffered.length - 1 - i) / duration) * 100 + "%";
+        buffer.innerHTML = '<style>.slider::after { width: ' + audioItemBufWidth + ';}</style>';
+        break;
+      }
+    }
+  }
+}
+
+audioRange.addEventListener("input", function () {
+  audioItem.currentTime = (this.value / 100) * audioItem.duration
+  let rangeValue = audioRange.value;
+  let color = 'background : linear-gradient(to right,#fff ' + rangeValue + '%,rgba(255,255,255,.5) ' + rangeValue + '%)!important;';
+  audioRange.style = color;
+});
+
+function getTime(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time - (minutes * 60))
+  let minuteValue;
+  let secondsValue;
+
+  if (minutes < 10) {
+    minuteValue = '0' + minutes;
+  } else {
+    minuteValue = minutes;
+  }
+
+  if (seconds < 10) {
+    secondsValue = '0' + seconds;
+  } else {
+    secondsValue = seconds;
+  }
+  return minuteValue + ':' + secondsValue
+}
+
+volumeRange.addEventListener("input", function () {
+  let vlmRangeVal = volumeRange.value;
+  let color = 'background : linear-gradient(to right,#fff ' + vlmRangeVal + '%,rgba(255,255,255,.5) ' + vlmRangeVal + '%)!important;';
+  volumeRange.style = color;
+
+  if (vlmRangeVal >= 51) {
+    volumeIcon.innerHTML = '<i class="fal fa-volume-up"></i>';
+  }
+  if (vlmRangeVal <= 50) {
+    volumeIcon.innerHTML = '<i class="fal fa-volume-down"></i>';
+  }
+  if (vlmRangeVal <= 30) {
+    volumeIcon.innerHTML = '<i class="fal fa-volume-off"></i>';
+  }
+  if (vlmRangeVal == 0) {
+    volumeIcon.innerHTML = '<i class="fal fa-volume-mute"></i>';
+  }
+  audioItem.volume = this.value / 100;
+});
+
+volumeIcon.addEventListener("click", function () {
+  if (flagVolume < 3) {
+    flagVolume++;
+  } else {
+    flagVolume = 1;
+  }
+
+  switch (flagVolume) {
+    case 1:
+      volumeIcon.innerHTML = '<i class="fal fa-volume-up"></i>';
+      volumeRange.value = '100';
+      audioItem.volume = 100 / 100;
+      color = 'background : linear-gradient(to right,#fff 100%,rgba(255,255,255,0.5) 0)!important;';
+      volumeRange.style = color;
+      break;
+    case 2:
+      volumeIcon.innerHTML = '<i class="fal fa-volume-mute"></i>';
+      volumeRange.value = '0';
+      audioItem.volume = 0 / 100;
+      color = 'background : linear-gradient(to right,#fff 0,rgba(255,255,255,0.5) 0)!important;';
+      volumeRange.style = color;
+      break;
+    case 3:
+      volumeIcon.innerHTML = '<i class="fal fa-volume-down"></i>';
+      volumeRange.value = '50';
+      audioItem.volume = 50 / 100;
+      color = 'background : linear-gradient(to right,#fff 50%,rgba(255,255,255,0.5) 50%)!important;';
+      volumeRange.style = color;
+      break;
+    default:
+      volumeIcon.innerHTML = '<i class="fal fa-volume-up"></i>';
+      volumeRange.value = '100';
+      audioItem.volume = 100 / 100;
+      color = 'background : linear-gradient(to right,#fff 100%,rgba(255,255,255,0.5) 0)!important;';
+      volumeRange.style = color;
+  }
+});
